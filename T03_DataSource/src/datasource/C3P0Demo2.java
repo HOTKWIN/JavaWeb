db0d2312043bd8d13b6c76b3f284e17825d3222c
kwh <kwh361@gmail.com>
2019-11-25 19:32:09 +0800

    commit3

--- T03_DataSource/src/datasource/C3P0Demo2.java
+++ T03_DataSource/src/datasource/C3P0Demo2.java
@@ -17,15 +17,21 @@ public class C3P0Demo2 {
 
         //1.获取DataSource,使用默认配置
         DataSource ds = new ComboPooledDataSource();
-
         //2.获取连接
-        for (int i = 0; i <= 11; i++) {
+        for (int i = 1; i <= 11; i++) {
             Connection conn = ds.getConnection();
             System.out.println(i + ":" + conn);
-//            if (i == 5)
-//                conn.close();//归还连接到连接池中
+            if (i == 5)
+                conn.close();
         }
-    }
 
-//    public static void testNameConfig(){}
+        //使用指定名称配置
+//        DataSource ds1 = new ComboPooledDataSource("otherc3p0");
+//        for (int i = 1; i <= 11; i++) {
+//            Connection conn = ds1.getConnection();
+//            System.out.println(i + ":" + conn);
+//            if (i == 5)
+//                conn.close();
+//        }
+    }
 }
